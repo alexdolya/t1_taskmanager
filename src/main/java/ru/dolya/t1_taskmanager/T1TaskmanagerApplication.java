e0eee7f8c3d33c232bf115c7c7448ee5b3f61961
alexdolya <alexdolya90@gmail.com>
2024-04-14 07:31:20 +0400

    initial

---
+++ src/main/java/ru/dolya/t1_taskmanager/T1TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package ru.dolya.t1_taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1TaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dolya/t1_taskmanager/T1TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolya.t1_taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
